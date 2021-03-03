8ddc4b65e0565889db1298db60ae45dca3d3cd4f
kawasima <kawasima1016@gmail.com>
2021-03-03 14:29:21 +0900

    init

---
+++ src/test/java/SetupTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SetupTest {
+    @Test
+    void test() {
+        Assertions.assertTrue(true, "セットアップ完了");
+    }
+}
